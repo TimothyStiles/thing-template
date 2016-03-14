745255112260bafca263b1ad67acc219c19c9dc0
TimothyStiles <TimothySStiles@gmail.com>
2016-03-13 21:37:38 -0400

    initial commit for th.ing/geom template.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject thing/lein-template "0.2.0"
+  :description "A template to generate thing projects."
+  :url "https://github.com/TimothyStiles/thing-template"
+  :license {:name "Apache Software Licesne"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :eval-in-leiningen true)
---
+++ resources/leiningen/new/thing/project.clj
@@ -0,0 +1,10 @@
+(defproject {{raw-name}} "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Apache Software License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [thi.ng/geom "0.0.908"]]
+  :main ^:skip-aot {{namespace}}
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
