5a1426942931b8dfd11931ff48c0925475e6b4e6
TimothyStiles <TimothySStiles@gmail.com>
2016-03-14 11:34:24 -0400

    fixed version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thing/lein-template "0.2.0"
+(defproject thing/lein-template "0.2.1"
   :description "A template to generate thing projects."
   :url "https://github.com/TimothyStiles/thing-template"
   :license {:name "Apache Software Licesne"
