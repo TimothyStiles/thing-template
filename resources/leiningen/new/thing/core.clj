7532534ebc8478ff0ecfd3dcb33ba161b4892fb4
TimothyStiles <TimothySStiles@gmail.com>
2016-03-25 15:08:40 -0400

    changed hello world to hello cube.

--- resources/leiningen/new/thing/core.clj
+++ resources/leiningen/new/thing/core.clj
@@ -1,7 +1,24 @@
 (ns {{namespace}}
-  (:gen-class))
+  (:gen-class)
+  (:require [thi.ng.geom.core :as g]
+            [thi.ng.geom.mesh.io :as mio]
+            [thi.ng.geom.cuboid :as cube]
+            [clojure.java.io :as io]))
+
+(defn save-stl
+  "Takes file path and mesh instance, saves mesh as STL"
+  [path mesh]
+  (with-open [out (io/output-stream path)]
+    (mio/write-stl
+     (mio/wrapped-output-stream out)
+     (g/tessellate mesh))))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Hello cube!"
   [& args]
-  (println "Hello, World!"))
+  (save-stl (g/as-mesh (g/center (cube/cuboid 1)))))
+
+;uncomment and evaluate in repl to get a 3D printable cube spit into this
+;project directory!.
+;(main)
